57b323a47d07880f1e025fb1e997af8b24f3f2ef
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-03-22 22:09:38 -0500

    Adding basic styling
    Rough styles to clean up the elements. Will add appealing stying later on.

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Balance({ getTotal, total }) {
   return (
     <div className="balance-container">
-      <h1>{total}</h1>
-      <button onClick={() => getTotal()}>Get total</button>
+      <h1>${total}</h1>
+      {/* <button onClick={() => getTotal()}>Get total</button> */}
     </div>
   );
 }
